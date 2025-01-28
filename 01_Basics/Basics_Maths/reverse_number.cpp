84e6920f48aa7fbb0d54fa684cf55922c6dfe3d9
Arindam2003 <dindaarindam000@gmail.com>
2025-01-28 22:29:39 +0530

    reverse_digit

---
+++ 01_Basics/Basics_Maths/reverse_number.cpp
@@ -0,0 +1,30 @@
+#include<bits/stdc++.h>
+#include<iostream>
+using namespace std;
+int number_count(int num)
+{
+    int count=0;
+    while (num)
+    {
+        num=num/10;
+        count++;
+    }
+    return count;
+}
+int main()
+{
+    int x;    //132
+    cin>>x;
+    int countDigit=number_count(x);   //3
+    int new_number=0;
+    while (x)
+    {
+        int rem;  //2
+        rem=x%10;  
+        new_number=new_number+rem*pow(10,countDigit-1);
+        countDigit--;
+        x=x/10;
+    }
+    cout<<new_number<<endl;
+    return 0;
+}
