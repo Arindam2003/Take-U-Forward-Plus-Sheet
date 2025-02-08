05e002e0c73a6e237563d4c59d679f76144ef6bf
Arindam2003 <dindaarindam000@gmail.com>
2025-02-08 14:00:06 +0530

    Check points

---
+++ 09_Stack_Queue/Q_using_stack.cpp
@@ -0,0 +1,9 @@
+#include<iostream>
+#include<stack>
+using namespace std;
+int main()
+{
+    //! using two stack to implemented the queue
+    // push normally
+    // pop -> check empty then pop otherwise input go to output
+}
