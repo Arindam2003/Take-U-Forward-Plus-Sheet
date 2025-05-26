6f6c67101893d35b128b47e3940deb46412ce278
Arindam2003 <dindaarindam000@gmail.com>
2025-05-26 23:44:46 +0530

    LL added

--- 4_Link_L_45/LL_check.cpp
+++ 4_Link_L_45/LL_check.cpp
@@ -1,24 +1,24 @@
-#include<iostream>
+#include <iostream>
 using namespace std;
 
-class Node {
-    public:
-        int data;
-        Node* next;
-        Node(int setData,Node* setNext)
-        {
-            data=setData;
-            next=setNext;
-        }
-        Node(int setData)
-        {
-            data=setData;
-            next=nullptr;
-        }
+class Node
+{
+public:
+    int data;
+    Node *next;
+    Node(int setData, Node *setNext)
+    {
+        data = setData;
+        next = setNext;
+    }
+    Node(int setData)
+    {
+        data = setData;
+        next = nullptr;
+    }
 };
 
-
-Node* convertArrtoLL(vector<int> arr)
+Node *convertArrtoLL(vector<int> arr)
 {
     Node *head = new Node(arr[0]);
     Node *mover = head;
@@ -31,56 +31,105 @@ Node* convertArrtoLL(vector<int> arr)
     return head;
 }
 
-Node* deleteHead(Node* &head)
+Node *deleteHead(Node *&head)
 {
-    Node* temp=head;
-    head=head->next;
+    Node *temp = head;
+    head = head->next;
     delete temp;
     return head;
 }
-Node* deleteLast(Node* &head)
+Node *deleteLast(Node *&head)
 {
-    Node* temp=head;
-    while(temp->next->next!=nullptr)
+    Node *temp = head;
+    while (temp->next->next != nullptr)
     {
-        temp=temp->next;
+        temp = temp->next;
     }
-    Node* del=temp->next;
-    temp->next=del->next;
+    Node *del = temp->next;
+    temp->next = del->next;
     delete del;
+    return head;
+}
+
+Node *insertAtKthPosition(Node *&head, int X, int K)
+{
+    Node *newnode = new Node(X);
+    if (head == nullptr)
+    {
+        head = newnode;
+        return head;
+    }
+    if (K == 1)
+    {
+        newnode->next = head;
+        head = newnode;
+        return head;
+    }
+    Node *temp = head;
+    int i = 1;
+    while (i < K - 1)
+    {
+        i++;
+        temp = temp->next;
+    }
+    Node *ins = temp->next;
+    temp->next = newnode;
+    newnode->next = ins;
+    return head;
+}
+Node *insertAtTail(Node *&head, int X)
+{
+    Node *newNode = new Node(X);
+    if (head == nullptr)
+    {
+        head = newNode;
+        return head;
+    }
+    Node *temp = head;
+    while (temp->next != nullptr)
+    {
+        temp = temp->next;
+    }
+    temp->next = newNode;
+    return head;
+}
+
+Node *insertAtHead(Node *&head, int X)
+{
+    Node *newnode = new Node(X);
+    newnode->next = head;
+    head = newnode;
     return head;
 }
 
 int main()
 {
-    vector<int> arr={12,3,4,2,0};
+    vector<int> arr = {12, 3, 4, 2, 0};
     cout << "Array to LL" << endl;
-    Node *head=convertArrtoLL(arr);
-    Node* temp=head;
-    while(temp!=nullptr)
+    Node *head = convertArrtoLL(arr);
+    Node *temp = head;
+    while (temp != nullptr)
     {
-        cout<<temp->data<<" ";
-        temp=temp->next;
+        cout << temp->data << " ";
+        temp = temp->next;
     }
-    cout<<endl;
+    cout << endl;
     //! After Head Delete
-    cout<<"After Delete Head"<<endl;
-    head=deleteHead(head);
-    temp=head;
-    while(temp!=nullptr)
+    cout << "After Delete Head" << endl;
+    head = deleteHead(head);
+    temp = head;
+    while (temp != nullptr)
     {
-        cout<<temp->data<<" ";
-        temp=temp->next;
+        cout << temp->data << " ";
+        temp = temp->next;
     }
     //! After Last Delete
-    cout<<"After Delete Head"<<endl;
-    head=deleteHead(head);
-    temp=head;
-    while(temp!=nullptr)
+    cout << "After Delete Last" << endl;
+    head = deleteLast(head);
+    temp = head;
+    while (temp != nullptr)
     {
-        cout<<temp->data<<" ";
-        temp=temp->next;
+        cout << temp->data << " ";
+        temp = temp->next;
     }
-    
 }
-
